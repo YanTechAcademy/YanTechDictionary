f23401ef02c1833aae1025495f6607d151a693fd
YanTechAcademy <153001628+YanTechAcademy@users.noreply.github.com>
2023-12-15 05:03:09 -0800

    15th Dec update

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const searchedWordEl = document.querySelector(".searched-word");
 const soundBtn = document.querySelector(".sound-logo");
 const resultContainer = document.querySelector(".result-container");
 
+const loader = document.querySelector(".loader-container");
+
+
+
+hideLoader();
+
+let speechUtObject;
+let text = "";
+
 // activating the form by the clicking on the trigger button
 trigger.addEventListener("click", () => {
   form.classList.add("active");
@@ -34,17 +43,33 @@ form.addEventListener("submit", (e) => {
 async function sendRequest(inputWord) {
   try {
     // sending the request using the fetch api
+
+
+     showLoader();
+
     let res = await fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`
     );
 
+    
+  
     // converting the response into json
-    let data = await res.json();
+    let data = await res.json();   
 
+    setTimeout(() => {
+      
+      hideLoader();
+      }, 10000);
+  
     console.log(data);
 
+   
     // calling the output word funtion to ouput the typed word
     displayWord(searchedWordEl, data[0].word);
+
+    // set the text to read
+
+    text = data[0].word + ". ";
 
     let audioIndex;
 
@@ -71,12 +96,18 @@ async function sendRequest(inputWord) {
       <h3>Pronunciation</h3>
 
       <ul class="phonetic">
-        <li>${data[0].phonetic}</li>
+        <li>${data[0].phonetic === undefined ? "" : data[0].phonetic}</li>
       </ul>
 
       <div class="line"></div>
 
     `;
+
+    //  set the text to speak
+
+    text += "Pronunciation. ";
+
+    text += `${data[0].phonetic === undefined ? "" : data[0].phonetic}`;
 
     // the example variable
     let example = "";
@@ -91,6 +122,11 @@ async function sendRequest(inputWord) {
 
 
       `;
+
+      // set text to speak
+
+      text += `${data[0].meanings[i].partOfSpeech}` + ". ";
+
       // resultContainer.innerHTML += "<ol>";
 
       // example Loop
@@ -110,10 +146,17 @@ async function sendRequest(inputWord) {
           
 
         `;
+
+        // set the text to peak
+
+        text += `${data[0].meanings[i].definitions[j].definition}` + " ";
+        text += `${example}` + " ";
       }
 
       // Synonym Loop
-      resultContainer.innerHTML += `<h4>Synomys:</h4>`;
+      resultContainer.innerHTML += `<h4>Synonyms:</h4>`;
+      text += "Synonyms. ";
+
       for (k = 0; k < data[0].meanings[i].synonyms.length; k++) {
         synonyms =
           data[0].meanings[i].synonyms[k] === undefined
@@ -125,10 +168,16 @@ async function sendRequest(inputWord) {
          
           
         `;
+
+        // set the text to speak
+
+        text += `${synonyms}` + ". ";
       }
 
       // Antonym Loop
       resultContainer.innerHTML += `<h4>Antonyms:</h4>`;
+      text += "Antonyms. ";
+
       for (q = 0; q < data[0].meanings[i].antonyms.length; q++) {
         antonym =
           data[0].meanings[i].antonyms[q] === undefined
@@ -139,9 +188,14 @@ async function sendRequest(inputWord) {
         <a href="#" class="antonym" data-antonym='${data[0].meanings[i].antonyms[q]}'>[ ${data[0].meanings[i].antonyms[q]} ] </a>
              
            `;
+
+        text += `${antonym}` + ". ";
       }
 
+      speechUtObject = new SpeechSynthesisUtterance(text);
+
       resultContainer.innerHTML += `<div class="line"></div>`;
+      console.log(text);
     }
   } catch (er) {
     alert(`Sorry, we could not find the word ${inputWord}, try another word !`);
@@ -181,7 +235,7 @@ function getSoundUrl(url) {
 
 // const synonym =
 //   document.querySelector(".synonym") === null
-//     ? ""
+// ? ""
 //     : document.querySelector(".synonym");
 
 // synonym.addEventListener("click", (e) => {
@@ -189,3 +243,45 @@ function getSoundUrl(url) {
 
 //   sendRequest(synonymData);
 // });
+
+let pauseBtn = document.querySelector(".pause-logo");
+let playBtn = document.querySelector(".play-logo");
+let resumeBtn = document.querySelector(".resume-logo");
+
+pauseBtn.style.display = "none";
+resumeBtn.style.display = "none";
+
+playBtn.addEventListener("click", () => {
+  window.speechSynthesis.speak(speechUtObject);
+  playBtn.style.display = "none";
+
+  pauseBtn.style.display = "inline-block";
+});
+
+pauseBtn.addEventListener("click", () => {
+ 
+  window.speechSynthesis.pause(speechUtObject);
+
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "inline-block";
+});
+
+resumeBtn.addEventListener("click", () => {
+
+  window.speechSynthesis.resume(speechUtObject);
+
+  resumeBtn.style.display = "none";
+
+  playBtn.style.display = "inline-block";
+});
+
+
+function hideLoader(){
+  loader.classList.add("remove");
+ }
+ 
+ function showLoader(){
+   loader.classList.remove("remove");
+  }
+  
+
